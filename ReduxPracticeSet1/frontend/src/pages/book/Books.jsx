fb44c287240f68919db2ce9aa1e0d626981107d9
7vTr0x7 <7vTrox7@gmail.com>
2024-08-28 14:48:13 +0530

    ReduxPracticeSet1 - frontend - pages - features - BookList

--- ReduxPracticeSet1/frontend/src/pages/book/Books.jsx
+++ ReduxPracticeSet1/frontend/src/pages/book/Books.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchBooks } from "./bookSlice";
+import BookList from "./features/BookList";
 
 const Books = () => {
   const dispatch = useDispatch();
@@ -15,7 +16,7 @@ const Books = () => {
     <>
       <div>{status === "Loading" && <p>Loading</p>}</div>
       <div>{error && <p>Error : {error}</p>}</div>
-      <div>{books && books.length > 0}</div>
+      <div>{books && books.length > 0 && <BookList books={books} />}</div>
     </>
   );
 };
