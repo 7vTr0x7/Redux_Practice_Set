33d5c98d548feffbde659ea8fcc3fa7bb2fe7775
7vTr0x7 <7vTrox7@gmail.com>
2024-08-28 16:31:58 +0530

    ReduxPracticeSet1 - backend

--- ReduxPracticeSet1/backend/db/db.connection.js
+++ ReduxPracticeSet1/backend/db/db.connection.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: "D:/redux/ReduxPracticeSet1/backend/.env" });
+require("dotenv").config();
 const mongoose = require("mongoose");
 
 const mongoURI = process.env.MONGODB;
