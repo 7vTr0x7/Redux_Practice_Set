74b0526eefa63999d05a9442ed92390f9d07d2a8
7vTr0x7 <7vTrox7@gmail.com>
2024-08-28 14:38:57 +0530

    ReduxPracticeSet1 - frontend - pages - booksSlice

---
+++ ReduxPracticeSet1/frontend/src/pages/book/bookSlice.js
@@ -0,0 +1,41 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const fetchBooks = createAsyncThunk("fetchBooks", async () => {
+  try {
+    const res = await fetch("http://localhost:4000/books");
+
+    if (!res.ok) {
+      console.log("Failed to get books");
+    }
+
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+const bookSlice = createSlice({
+  name: "books",
+  initialState: {
+    books: [],
+    status: "idle",
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchBooks.pending, (state, action) => {
+      state.status = "Loading";
+    });
+    builder.addCase(fetchBooks.fulfilled, (state, action) => {
+      state.status = "Success";
+      state.books = action.payload;
+    });
+    builder.addCase(fetchBooks.rejected, (state, action) => {
+      state.status = "Failed";
+      state.error = "Failed to get books";
+    });
+  },
+});
+
+export default bookSlice.reducer;
