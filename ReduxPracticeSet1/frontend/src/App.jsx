6ccf2220cf315cafaf19c51fd811a201c7963cc3
7vTr0x7 <7vTrox7@gmail.com>
2024-08-28 15:20:57 +0530

    ReduxPracticeSet1 - frontend - Routes

--- ReduxPracticeSet1/frontend/src/App.jsx
+++ ReduxPracticeSet1/frontend/src/App.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import Books from "./pages/book/Books";
 
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import BookForm from "./pages/book/features/BookForm";
+
 const App = () => {
   return (
     <>
       <Books />
+
+      <Router>
+        <Routes>
+          <Route path="/" to={<Books />} />
+          <Route path="/bookFrom" to={<BookForm />} />
+        </Routes>
+      </Router>
     </>
   );
 };
