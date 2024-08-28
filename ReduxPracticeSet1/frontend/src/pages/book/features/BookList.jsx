91a13bc122f7089b35b1edfc0ad1e3ef899c2341
7vTr0x7 <7vTrox7@gmail.com>
2024-08-28 15:17:40 +0530

    ReduxPracticeSet1 - frontend - pages - features - BookList

--- ReduxPracticeSet1/frontend/src/pages/book/features/BookList.jsx
+++ ReduxPracticeSet1/frontend/src/pages/book/features/BookList.jsx
@@ -11,7 +11,10 @@ const BookList = ({ books }) => {
 
   return (
     <>
-      <p className="fs-2 fw-bold">Book List</p>
+      <div className="d-flex col-md-6 justify-content-between">
+        <p className="fs-2 fw-bold">Book List</p>
+        <button className="btn btn-primary h-50 mt-3">Add Book</button>
+      </div>
       <div className="col-md-6">
         <ul className="list-group">
           {books &&
@@ -23,14 +26,18 @@ const BookList = ({ books }) => {
                 <p>
                   <b>Author:</b> {book.author}
                 </p>
-                <button
-                  onClick={() => deleteHandler(book._id)}
-                  className="btn btn-danger float-end mx-2">
-                  Delete
-                </button>
+
                 <p>
                   <b>Summary:</b> {book.summary}
                 </p>
+                <div>
+                  <button className="btn btn-primary ">Edit</button>
+                  <button
+                    onClick={() => deleteHandler(book._id)}
+                    className="btn btn-danger mx-3">
+                    Delete
+                  </button>
+                </div>
               </li>
             ))}
         </ul>
