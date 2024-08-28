77309b1df2cdce7efb7a1153b9372ee46d808880
7vTr0x7 <7vTrox7@gmail.com>
2024-08-28 15:05:45 +0530

    ReduxPracticeSet1 - frontend - pages - features - BookList

--- ReduxPracticeSet1/frontend/src/pages/book/Books.jsx
+++ ReduxPracticeSet1/frontend/src/pages/book/Books.jsx
@@ -6,18 +6,17 @@ import BookList from "./features/BookList";
 const Books = () => {
   const dispatch = useDispatch();
   const { books, status, error } = useSelector((state) => state.books);
-  console.log(books);
 
   useEffect(() => {
     dispatch(fetchBooks());
   }, []);
 
   return (
-    <>
+    <div className="container my-5">
       <div>{status === "Loading" && <p>Loading</p>}</div>
       <div>{error && <p>Error : {error}</p>}</div>
       <div>{books && books.length > 0 && <BookList books={books} />}</div>
-    </>
+    </div>
   );
 };
 
--- ReduxPracticeSet1/frontend/src/pages/book/features/BookList.jsx
+++ ReduxPracticeSet1/frontend/src/pages/book/features/BookList.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
-const BookList = () => {
-  return <div>BookList</div>;
+const BookList = ({ books }) => {
+  console.log(books);
+  return (
+    <>
+      <p className="fs-2 fw-bold">Book List</p>
+      <div className="col-md-6">
+        <ul className="list-group">
+          {books &&
+            books.map((book) => (
+              <li key={book._id} className="list-group-item px-3 py-2">
+                <p>
+                  <b>Name:</b> {book.title}
+                </p>
+                <p>
+                  <b>Author:</b> {book.author}
+                </p>
+                <p>
+                  <b>Summary:</b> {book.summary}
+                </p>
+              </li>
+            ))}
+        </ul>
+      </div>
+    </>
+  );
 };
 
 export default BookList;
