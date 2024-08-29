fcb5f4200f0efbbee7e9daf26fcda9135b4280c5
7vTr0x7 <7vTrox7@gmail.com>
2024-08-29 12:29:13 +0530

    ReduxPracticeSet2 - backend

--- ReduxPracticeSet2/backend/index.js
+++ ReduxPracticeSet2/backend/index.js
@@ -56,6 +56,21 @@ app.post("/movies", async (req, res) => {
   }
 });
 
+app.post("/movies/:id", async (req, res) => {
+  try {
+    const movie = await Movies.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (movie) {
+      res.json(movie);
+    } else {
+      res.status(404).json({ error: "Movie not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: `Failed to add movie ${error}` });
+  }
+});
+
 const PORT = 4000;
 app.listen(PORT, () => {
   console.log(`Server Running on ${PORT}`);
