e7179f5ed5c52afa49f01b9ef36c2603278523ba
7vTr0x7 <7vTrox7@gmail.com>
2024-08-29 13:21:11 +0530

    ReduxPracticeSet2 - frontend - pages - movie - features - MovieList

--- ReduxPracticeSet2/frontend/src/pages/movies/features/MovieList.jsx
+++ ReduxPracticeSet2/frontend/src/pages/movies/features/MovieList.jsx
@@ -1,7 +1,36 @@
 import React from "react";
 
-const MovieList = () => {
-  return <div>MovieList</div>;
+const MovieList = ({ movies }) => {
+  return (
+    <>
+      <div>
+        <p className="fs-2 fw-bold">Movie List</p>
+        <div className="col-md-6">
+          <ul className="list-group">
+            {movies.map((movie) => (
+              <li className="list-group-item" key={movie._id}>
+                <p>
+                  <b>Title: </b>
+                  {movie.title}
+                </p>
+                <p>
+                  <b>Director: </b>
+                  {movie.director}
+                </p>
+                <p>
+                  <b>Genre: </b>
+                  {movie.genre.join(", ")}
+                </p>
+
+                <button className="btn btn-primary ">Edit</button>
+                <button className="btn btn-danger mx-3">Delete</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default MovieList;
