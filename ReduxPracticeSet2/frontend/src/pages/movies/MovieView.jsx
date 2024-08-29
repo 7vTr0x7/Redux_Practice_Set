189f6794bc5d9b08a9a6f1344086c69731e3c88d
7vTr0x7 <7vTrox7@gmail.com>
2024-08-29 13:13:46 +0530

    ReduxPracticeSet2 - frontend - pages - movie - MovieView

--- ReduxPracticeSet2/frontend/src/pages/movies/MovieView.jsx
+++ ReduxPracticeSet2/frontend/src/pages/movies/MovieView.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import MovieList from "./features/MovieList";
+import { fetchMovies } from "./movieSlice";
 
 const MovieView = () => {
   const dispatch = useDispatch();
   const { movies, status, error } = useSelector((state) => state.movies);
+
+  useEffect(() => {
+    dispatch(fetchMovies());
+  }, []);
 
   return (
     <div className="container my-4">
@@ -14,7 +19,7 @@ const MovieView = () => {
         )}
       </div>
       <div>{error && <p className="fs-3 fw-semibold">Error : {error} </p>}</div>
-      <div>{movies && movies.length > 0 && <MovieList />}</div>
+      <div>{movies && movies.length > 0 && <MovieList movies={movies} />}</div>
     </div>
   );
 };
