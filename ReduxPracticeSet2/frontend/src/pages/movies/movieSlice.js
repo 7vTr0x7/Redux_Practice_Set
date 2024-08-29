144b3d3c1a33e44ceb97ee884e53a3ef703739ec
7vTr0x7 <7vTrox7@gmail.com>
2024-08-29 13:06:10 +0530

    ReduxPracticeSet2 - frontend - pages - movie - movieSlice

--- ReduxPracticeSet2/frontend/src/pages/movies/movieSlice.js
+++ ReduxPracticeSet2/frontend/src/pages/movies/movieSlice.js
@@ -1,4 +1,21 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const fetchMovies = createAsyncThunk("fetchMovies", async () => {
+  try {
+    const res = await fetch(
+      "https://redux-practice-set-2-backend.vercel.app/movies"
+    );
+
+    if (!res.ok) {
+      console.log("Failed to get movies");
+    }
+
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 const movieSlice = createSlice({
   name: "movies",
@@ -8,6 +25,19 @@ const movieSlice = createSlice({
     error: null,
   },
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchMovies.pending, (state, action) => {
+      state.status = "Loading";
+    });
+    builder.addCase(fetchMovies.fulfilled, (state, action) => {
+      state.status = "Success";
+      state.movies = action.payload;
+    });
+    builder.addCase(fetchMovies.rejected, (state, action) => {
+      state.status = "Failed";
+      state.error = "Failed to get movies";
+    });
+  },
 });
 
 export default movieSlice.reducer;
